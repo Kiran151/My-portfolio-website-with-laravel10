9a5bda6f547a974157d88b38fe1e9b2dbc80f66a
Kiran151 <kiran1999gopal@gmail.com>
2023-09-26 12:09:44 +0530

    some changes

--- resources/views/admin/index.blade.php
+++ resources/views/admin/index.blade.php
@@ -41,7 +41,7 @@
                                 </div>
                                 <div class="col-6">
                                     <div class="text-end">
-                                        <h3 class="text-dark mt-1">$<span data-plugin="counterup">58,947</span></h3>
+                                        <h3 class="text-dark mt-1">$<span data-plugin="counterup">58,946</span></h3>
                                         <p class="text-muted mb-1 text-truncate">Total Revenue</p>
                                     </div>
                                 </div>
